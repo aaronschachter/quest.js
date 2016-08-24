d13181c1d25e73e9c5cfe3bb1ca5e68a7bf41656
Aaron Schachter <aaronschachter@gmail.com>
2016-08-23 19:17:22 -0700

    Imports questions into firebase

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 var firebase = require("firebase");
+var request = require('request');
 
-// Initialize Firebase
-var config = {
+var firebaseAuth = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseAuth);
 var database = firebase.database();
-console.log("hi");
+
+var url = process.env.IVQ_API_BASEURL;
+request(url + 'questions', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var questions = JSON.parse(body);
+    for (var i = 0; i < questions.length; i++) {
+      var question = questions[i];
+      console.log(question.id + ':' + question.question_title);
+      firebase.database().ref('questions/' + question.id).set({
+        title: question.question_title
+      });
+    }
+  }
+});
